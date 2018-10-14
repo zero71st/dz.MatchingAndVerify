cc326bb90af96aaa7a0821804c3a535e695782dc
Kasem Wongsa <zero71st@gmail.com>
2018-10-14 22:01:21 +0700

    added IRepository

---
+++ dz.MachingAndVerify.Core/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using dz.MatchingAndVerify.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz.MatchingAndVerify.Core.Interfaces.Repositories
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
