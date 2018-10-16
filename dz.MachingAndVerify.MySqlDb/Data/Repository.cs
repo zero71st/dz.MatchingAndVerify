29543e06c9d06f0f95aa2c481689722243c9ed76
kasem <kasem.w@dzcard.com>
2018-10-16 08:24:07 +0700

    Added empty repository.

---
+++ dz.MachingAndVerify.MySqlDb/Data/Repository.cs
@@ -0,0 +1,38 @@
+﻿using dz.MatchingAndVerify.Core.Entities;
+using dz.MatchingAndVerify.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz.MachingAndVerify.MySqlDb.Data
+{
+    public class Repository<T>:IRepository<T> where T: BaseEntity
+    {
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
