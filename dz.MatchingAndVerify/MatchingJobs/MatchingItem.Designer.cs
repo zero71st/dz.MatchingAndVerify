c28fd5525198ff827d27781733c24de3919a01c5
kasem <kasem.w@dzcard.com>
2018-10-19 15:18:56 +0700

    Added pending and matched.

--- dz.MatchingAndVerify/MatchingJobs/MatchingItem.Designer.cs
+++ dz.MatchingAndVerify/MatchingJobs/MatchingItem.Designer.cs
@@ -42,17 +42,22 @@
             this.lbComponent3 = new System.Windows.Forms.Label();
             this.tbComponent4 = new System.Windows.Forms.TextBox();
             this.lbComponent4 = new System.Windows.Forms.Label();
-            this.gvMatchingItems = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.gvMatchingItems)).BeginInit();
+            this.gvPending = new System.Windows.Forms.DataGridView();
+            this.gvMatched = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvPending)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMatched)).BeginInit();
             this.SuspendLayout();
             // 
             // btSave
             // 
-            this.btSave.Location = new System.Drawing.Point(601, 589);
+            this.btSave.Location = new System.Drawing.Point(790, 589);
             // 
             // btCancel
             // 
-            this.btCancel.Location = new System.Drawing.Point(682, 589);
+            this.btCancel.Location = new System.Drawing.Point(871, 589);
             // 
             // label3
             // 
@@ -114,7 +119,7 @@
             // lbComponen1
             // 
             this.lbComponen1.AutoSize = true;
-            this.lbComponen1.Location = new System.Drawing.Point(35, 136);
+            this.lbComponen1.Location = new System.Drawing.Point(35, 116);
             this.lbComponen1.Name = "lbComponen1";
             this.lbComponen1.Size = new System.Drawing.Size(99, 17);
             this.lbComponen1.TabIndex = 9;
@@ -122,15 +127,15 @@
             // 
             // tbComponent1
             // 
-            this.tbComponent1.Location = new System.Drawing.Point(155, 133);
+            this.tbComponent1.Location = new System.Drawing.Point(155, 113);
             this.tbComponent1.Name = "tbComponent1";
-            this.tbComponent1.Size = new System.Drawing.Size(602, 23);
+            this.tbComponent1.Size = new System.Drawing.Size(690, 23);
             this.tbComponent1.TabIndex = 10;
             // 
             // lbComponent2
             // 
             this.lbComponent2.AutoSize = true;
-            this.lbComponent2.Location = new System.Drawing.Point(35, 175);
+            this.lbComponent2.Location = new System.Drawing.Point(35, 145);
             this.lbComponent2.Name = "lbComponent2";
             this.lbComponent2.Size = new System.Drawing.Size(99, 17);
             this.lbComponent2.TabIndex = 11;
@@ -138,22 +143,22 @@
             // 
             // tbComponent2
             // 
-            this.tbComponent2.Location = new System.Drawing.Point(155, 172);
+            this.tbComponent2.Location = new System.Drawing.Point(155, 142);
             this.tbComponent2.Name = "tbComponent2";
-            this.tbComponent2.Size = new System.Drawing.Size(602, 23);
+            this.tbComponent2.Size = new System.Drawing.Size(690, 23);
             this.tbComponent2.TabIndex = 12;
             // 
             // tbComponent3
             // 
-            this.tbComponent3.Location = new System.Drawing.Point(155, 211);
+            this.tbComponent3.Location = new System.Drawing.Point(155, 171);
             this.tbComponent3.Name = "tbComponent3";
-            this.tbComponent3.Size = new System.Drawing.Size(602, 23);
+            this.tbComponent3.Size = new System.Drawing.Size(690, 23);
             this.tbComponent3.TabIndex = 13;
             // 
             // lbComponent3
             // 
             this.lbComponent3.AutoSize = true;
-            this.lbComponent3.Location = new System.Drawing.Point(35, 214);
+            this.lbComponent3.Location = new System.Drawing.Point(35, 174);
             this.lbComponent3.Name = "lbComponent3";
             this.lbComponent3.Size = new System.Drawing.Size(99, 17);
             this.lbComponent3.TabIndex = 14;
@@ -161,34 +166,75 @@
             // 
             // tbComponent4
             // 
-            this.tbComponent4.Location = new System.Drawing.Point(155, 253);
+            this.tbComponent4.Location = new System.Drawing.Point(155, 200);
             this.tbComponent4.Name = "tbComponent4";
-            this.tbComponent4.Size = new System.Drawing.Size(602, 23);
+            this.tbComponent4.Size = new System.Drawing.Size(690, 23);
             this.tbComponent4.TabIndex = 15;
             // 
             // lbComponent4
             // 
             this.lbComponent4.AutoSize = true;
-            this.lbComponent4.Location = new System.Drawing.Point(35, 256);
+            this.lbComponent4.Location = new System.Drawing.Point(35, 203);
             this.lbComponent4.Name = "lbComponent4";
             this.lbComponent4.Size = new System.Drawing.Size(99, 17);
             this.lbComponent4.TabIndex = 16;
             this.lbComponent4.Text = "Component 4";
             // 
-            // gvMatchingItems
+            // gvPending
             // 
-            this.gvMatchingItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvMatchingItems.Location = new System.Drawing.Point(38, 310);
-            this.gvMatchingItems.Name = "gvMatchingItems";
-            this.gvMatchingItems.Size = new System.Drawing.Size(719, 256);
-            this.gvMatchingItems.TabIndex = 17;
+            this.gvPending.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvPending.Location = new System.Drawing.Point(38, 269);
+            this.gvPending.Name = "gvPending";
+            this.gvPending.Size = new System.Drawing.Size(924, 142);
+            this.gvPending.TabIndex = 17;
+            // 
+            // gvMatched
+            // 
+            this.gvMatched.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMatched.Location = new System.Drawing.Point(38, 441);
+            this.gvMatched.Name = "gvMatched";
+            this.gvMatched.Size = new System.Drawing.Size(924, 142);
+            this.gvMatched.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(35, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Pending";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(35, 422);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 16);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Matched";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(851, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 52);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MatchingItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(779, 633);
-            this.Controls.Add(this.gvMatchingItems);
+            this.ClientSize = new System.Drawing.Size(968, 633);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.gvMatched);
+            this.Controls.Add(this.gvPending);
             this.Controls.Add(this.lbComponent4);
             this.Controls.Add(this.tbComponent4);
             this.Controls.Add(this.lbComponent3);
@@ -222,8 +268,13 @@
             this.Controls.SetChildIndex(this.lbComponent3, 0);
             this.Controls.SetChildIndex(this.tbComponent4, 0);
             this.Controls.SetChildIndex(this.lbComponent4, 0);
-            this.Controls.SetChildIndex(this.gvMatchingItems, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.gvMatchingItems)).EndInit();
+            this.Controls.SetChildIndex(this.gvPending, 0);
+            this.Controls.SetChildIndex(this.gvMatched, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.gvPending)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMatched)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,6 +296,10 @@
         private System.Windows.Forms.Label lbComponent3;
         private System.Windows.Forms.TextBox tbComponent4;
         private System.Windows.Forms.Label lbComponent4;
-        private System.Windows.Forms.DataGridView gvMatchingItems;
+        private System.Windows.Forms.DataGridView gvPending;
+        private System.Windows.Forms.DataGridView gvMatched;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
     }
 }
--- dz.MatchingAndVerify/MatchingJobs/MatchingItem.cs
+++ dz.MatchingAndVerify/MatchingJobs/MatchingItem.cs
@@ -19,7 +19,11 @@ namespace dz.MatchingAndVerify.MatchingJobs
 {
     public partial class MatchingItem : DetailBase
     {
-        private List<MatchingComponentModel> _models = new List<MatchingComponentModel>();
+        private List<MatchingComponentModel> _allCards;
+        private List<MatchingComponentModel> _pendingCards;
+        private List<MatchingComponentModel> _matchedCards;
+
+        private MatchingComponentModel _currentCard;
 
         private IRepository<MatchingJob> _jobRepository;
         private JobService _uidJobService;
@@ -33,7 +37,28 @@ namespace dz.MatchingAndVerify.MatchingJobs
             _jobRepository = new Repository<MatchingJob>(_matchingDb);
 
             _uidJobService = new JobService(_uidDb);
+
+            tbComponent1.KeyDown += tbComponent1_KeyDown;
+            tbComponent2.KeyDown += tbComponent2_KeyDown;
+            tbComponent3.KeyDown += tbComponent3_KeyDown;
         }
+
+        void tbComponent3_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void tbComponent2_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void tbComponent1_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
 
         public MatchingItem(int jobId):this()
         {
@@ -42,23 +67,27 @@ namespace dz.MatchingAndVerify.MatchingJobs
             var cards = _uidJobService.GetItemsByJobID(job.JobId).ToList();
             var items = job.MatchItems.ToList();
 
-            _models = (from c in cards
-                      join x in items 
-                      on c.ID equals x.JobItemId 
-                      select new MatchingComponentModel 
-                      { 
-                          JobItemId = x.JobItemId,
-                          Component1 = c.UID,
-                          Component2 = c.MaskedCardNumber,
-                          Status = Status.New
-                      }).ToList();
+            _allCards = (from c in cards
+                         join x in items
+                         on c.ID equals x.JobItemId
+                         select new MatchingComponentModel
+                         {
+                             JobItemId = x.JobItemId,
+                             Component1 = c.UID,
+                             Component2 = c.MaskedCardNumber,
+                             Status = Status.New
+                         }).ToList();
 
-            LoadJobItems();
+            ReloadJobItems();
         }
 
-        private void LoadJobItems()
+        private void ReloadJobItems()
         {
-            gvMatchingItems.DataSource = _models;
+            _pendingCards = _allCards.Where(a => a.Status == Status.New).ToList();
+            _matchedCards = _allCards.Where(a => a.Status == Status.Matched).ToList();
+
+            gvPending.DataSource = _allCards;
+            gvMatched.DataSource = _matchedCards;
         }
 
         private void MatchingItem_Load(object sender, EventArgs e)
